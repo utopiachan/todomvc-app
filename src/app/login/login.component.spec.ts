a94b91b64b492a7e5e5465f0af600d53c6ebb2a1
utopiachan <whysosiri@hotmail.com>
2023-03-05 22:01:22 +0800

    auto test bug fix

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,12 +2,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { By } from '@angular/platform-browser';
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+
+  function updateForm(userName: string, userPassword: string) {
+    fixture.componentInstance.userName = userName;
+    fixture.componentInstance.password = userPassword;
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -28,4 +34,12 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should set user name and password to unittest", () => {
+    updateForm('unittest', 'unittest');
+    fixture.detectChanges();
+    expect(component.userName).toEqual('unittest');
+    expect(component.password).toEqual('unittest');
+  });
+
 });
